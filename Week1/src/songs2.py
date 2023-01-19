3a546f3740fda067a93c8a43330d79afbeae6941
Orion Grieco <46628700+Orion-Grieco@users.noreply.github.com>
2023-01-19 09:04:40 -0500

    Update songs2.py

--- Week1/src/songs2.py
+++ Week1/src/songs2.py
@@ -1,6 +1,17 @@
 def compute():
-	songs = [1,2,5,4,3]
-	# add your code here to compute min
+	song_list ={
+    "1": 156,
+    "2": 141,
+    "3": 35,
+    "4": 94,
+    "5": 88,
+    "6": 61,
+    "7": 111,
+    "8": 77
+}
+	min_key = [key for key in song_list if song_list[key] == min(song_list.values())]
+	min_key = str(min_key)
+	print("Song",min_key[2],":",min(song_list.values()))
 	
 if __name__ == "__main__":
     compute()
