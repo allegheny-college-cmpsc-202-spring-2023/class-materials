971bbd2a137dcab2dbb750787427e038b82c7308
Orion Grieco <Orion.grieco@Gmail.com>
2023-01-19 09:10:39 -0500

    e

--- Week1/src/songs1.py
+++ Week1/src/songs1.py
@@ -1,6 +1,12 @@
 def compute():
-	songs = [1,2,5,4,3]
-	# add your code here to compute max
-	
+    songs = [1, 2, 5, 4, 3]
+    # add your code here to compute max
+    temp = songs[0]
+    for i in songs:
+        if i < temp:
+            temp = songs[i]
+    print(temp)
+
+
 if __name__ == "__main__":
     compute()
