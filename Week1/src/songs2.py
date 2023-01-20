6fa153a0d4afe6fe37a36a50cb6a0f83f2c0387c
Orion Grieco <Orion.grieco@Gmail.com>
2023-01-20 15:04:27 -0500

    Update songs2.py

--- Week1/src/songs2.py
+++ Week1/src/songs2.py
@@ -9,9 +9,10 @@ def compute():
         "7": 111,
         "8": 77,
     }
-	min_key = [key for key in song_list if song_list[key] == min(song_list.values())]
-	min_key = str(min_key)
-	print("Song", min_key[2], ":", min(song_list.values()))
+    min_key = [key for key in song_list if song_list[key] == min(song_list.values())]
+    min_key = str(min_key)
+    print("Song", min_key[2], ":", min(song_list.values()))
+
 
 if __name__ == "__main__":
     compute()
