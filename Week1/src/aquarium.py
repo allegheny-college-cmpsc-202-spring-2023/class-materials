522bd9bdc96cf1948ab4cbcd940eb3a7d47c430c
Orion Grieco <Orion.grieco@Gmail.com>
2023-01-20 15:21:17 -0500

    Update aquarium.py

--- Week1/src/aquarium.py
+++ Week1/src/aquarium.py
@@ -1,10 +1,12 @@
 def compute():
-	# assume total 10 tanks 
-	for i in range(1,10):
-		tanks = i
-		turtles = tanks+1
-		# complete line 6 below by adding the right conditions inside the if block ...
-		if ():
-			print(turtles, tanks)
+    # assume total 10 tanks
+    for i in range(1, 11):
+        tanks = i
+        turtles = tanks + 1
+        # complete line 6 below by adding the right conditions inside the if block ...
+        if tanks > 1 and turtles / (tanks - 1) == 2:
+            print(turtles, tanks)
+
+
 if __name__ == "__main__":
     compute()
