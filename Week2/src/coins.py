199ab2d6470e642cbcd4b9dc75058fb896166a17
Orion Grieco <Orion.grieco@Gmail.com>
2023-01-26 11:24:08 -0500

    Update coins.py

--- Week2/src/coins.py
+++ Week2/src/coins.py
@@ -26,20 +26,49 @@ def generate(size):
 def detect(no_of_coins):
     pos = 0
     # add your logic here
-    for i in range(0, no_of_coins - 1):
+    for i in range(no_of_coins - 1):
+        for j in range(no_of_coins - 1):
+            if coins[j] < coins[i]:
+                pos = coins[j]
+            elif coins[i] < coins[j]:
+                pos = coins[i]
+                temp = coins.index(pos)
         pass
-    return pos
+    return pos, temp
+
+
+def detect2(low, high):
+    pos = 0
+    # mid_point = len(coins) // 2
+    # left = coins[:mid_point]  # until mid_point index
+    # right = coins[mid_point : 2 * mid_point]
+    first = range(low, (low + high) / 2)
+    second = range((low + high) / 2, high)
+    while len(coins) > 1:
+        if high - low == 1:
+            if first < second:
+                return low
+            else:
+                return high
+        else:
+            if first < second:
+                return detect2(low, (low + high) / 2)
+            else:
+                return detect2((low + high) / 2, high)
 
 
 if __name__ == "__main__":
-    # start = time.time()
+    start = time.time()
     size = 16  # number of coins is set here, can be any number in power of two's
     generate(size)
     print(coins)
-    pos = detect(size)
+    # pos = detect(size)
+    pos = detect2(0, 17)
+    print(f"Weight and location of the defective coin: {pos}")
+    # print(type(pos))
     # print("position(starting at 1):", pos)
     # print("weight:",coins[pos-1])
 
-    # end = time.time()
-    # elapsed_time = (end - start)
-    # print('Execution time:', elapsed_time, 'seconds')
+    end = time.time()
+    elapsed_time = end - start
+    print("Execution time:", elapsed_time, "seconds")
